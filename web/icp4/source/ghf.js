7d875f5028d36592c08a2e7a74e042c6668e2248
sai varun <gadhewarsaivarun78@gmail.com>
2019-09-14 15:42:01 -0500

    ICP4

---
+++ web/icp4/source/ghf.js
@@ -0,0 +1,47 @@
+function getGithubInfo(user) {
+    //1. Create an instance of XMLHttpRequest class and send a GET request using it. The function should finally return the object(it now contains the response!)
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            showUser(JSON.parse(this.responseText));
+        }
+        else if (this.readyState == 4) {
+            noSuchUser(user)
+        }
+    };
+    xhttp.open("GET", "https://api.github.com/users/" + user, true);
+    xhttp.send();
+}
+
+function showUser(user) {
+    $("#h2txt").text(user.login)
+    $("#avatar").append('<img src="'+user.avatar_url+'" width="100px" height="100px"/>')
+    $("#information").append('<a href="'+user.html_url+'">Github Url</a></br><a href="'+user.repos_url+'">Repo Url</a>')
+    //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+
+}
+
+function noSuchUser(username) {
+    //3. set the elements such that a suitable message is displayed
+
+    $("#h2txt").text("no such profile")
+}
+
+
+$(document).ready(function () {
+
+    $(document).on('keypress', '#username', function (e) {
+        //check if the enter(i.e return) key is pressed
+        $("#h2txt").html("")
+        $("#avatar").html("")
+        $("#information").html("")
+        if (e.which == 13) {
+            //get what the user enters
+            username = $(this).val();
+            //reset the text typed in the input
+            $(this).val("");
+            //get the user's information and store the respsonse
+            getGithubInfo(username);
+        }
+    })
+});
