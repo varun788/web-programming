a7cfbc335be2bb38968be7f90493e7a70913e3d4
sai varun <gadhewarsaivarun78@gmail.com>
2019-09-21 16:33:08 -0500

    ICP5

---
+++ web/ICP5/todo/src/app/app.component.ts
@@ -0,0 +1,38 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ICP5';
+
+  newTodo: string;
+  todos: any;
+  todoObj: any;
+
+  /**  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  } **/
+
+  constructor() {
+    this.newTodo = '';
+    this.todos = [];
+  }
+
+  addTodo(event) {
+    this.todoObj = {
+      newTodo: this.newTodo,
+      /*completed: false*/
+    }
+    this.todos.push(this.todoObj);
+    this.newTodo = '';
+    event.preventDefault();
+  }
+
+  deleteTodo(index) {
+    this.todos.splice(index, 1);
+  }
+}
