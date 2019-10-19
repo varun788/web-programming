fe1029d255daf3de8a99b01b1a8834d12b21f5a9
varun788 <gadhewarsaivarun78@gmail.com>
2019-10-19 16:07:03 -0500

    Add files via upload

---
+++ Mobile/ICP1/MainActivity.java
@@ -0,0 +1,65 @@
+package com.csee5590.helloworldapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void checkCredentials(View v)
+    {
+        EditText usernameCtrl = (EditText)findViewById(R.id.txt_uname);
+        EditText passwordCtrl = (EditText)findViewById(R.id.txt_Pwd);
+        TextView errorText = (TextView)findViewById(R.id.lbl_Error);
+        String userName = usernameCtrl.getText().toString();
+        String password = passwordCtrl.getText().toString();
+
+        boolean validationFlag = false;
+        //Verify if the username and password are not empty.
+        if(!userName.isEmpty() && !password.isEmpty()) {
+            if(userName.equals("admin") && password.equals("admin")) {
+                validationFlag = true;
+            }
+        }
+        if(!validationFlag)
+        {
+            errorText.setVisibility(View.VISIBLE);
+            Toast.makeText(getApplicationContext(),"Invaild details",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            //This code redirects the from login page to the home page.
+            Intent redirect = new Intent(MainActivity.this, MainPageActivity.class);
+            startActivity(redirect);
+        }
+
+    }
+}
---
+++ Mobile/ICP1/MainPageActivity.java
@@ -0,0 +1,36 @@
+package com.csee5590.helloworldapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void LogOut(View v) {
+        Intent intent = new Intent(MainPageActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
